a1c25336063493bef958920beb3a0c03aa9c8356
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2017-08-20 20:55:28 +0200

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -1,5 +1,15 @@
 function search() {
-	var pos=0;
+	
+	if(document.getElementById(array[0]).innerHTML==""){
+	document.getElementById(array[0]).innerHTML=document.getElementById(array[1]).innerHTML;
+	document.getElementById(array[1]).innerHTML="";
+	}
+	if(document.getElementById(array[1]).innerHTML==""){
+	document.getElementById(array[1]).innerHTML=document.getElementById(array[2]).innerHTML;
+ 	document.getElementById(array[2]).innerHTML="";
+	}
+		
+    var pos=0;
     var x = document.getElementById("myText").value;
     for(var i=0;i<arr.length;i++){
      if (arr[i].includes(x) && x!="") {
@@ -26,5 +36,4 @@ function search() {
      document.getElementById(array[pos]).innerHTML="";
      }
 }
-    
 }
