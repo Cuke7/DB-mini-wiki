c9a6bf174f9d6715de603870ea97a5d3373b08b0
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2017-08-21 13:16:42 +0200

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -24,4 +24,8 @@ function search() {
 		document.getElementById(array[3]).innerHTML="";
 		document.getElementById(array[4]).innerHTML="";
 	}
+	if(document.getElementById(array[1]).innerHTML==""){
+		document.getElementById(array[1]).innerHTML=document.getElementById(array[2]).innerHTML;
+		document.getElementById(array[2]).innerHTML="";
+	}
 }
