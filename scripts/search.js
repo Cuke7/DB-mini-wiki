d8783be0e01066c13e674bf6d5d340a4be2e71ca
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2017-08-21 13:01:34 +0200

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -2,25 +2,15 @@ function search() {
 		
     var pos=0;
     var x = document.getElementById("myText").value;
-    for(var i=0;i<arr.length;i++){
+    for(var i=0;i<Character.length;i++){
      if (arr[i].includes(x) && x!="") {
      //In the array!
      document.getElementById(array[pos]).setAttribute("href",x);
-     document.getElementById(array[pos]).innerHTML=arr[i];
+     document.getElementById(array[pos]).innerHTML=Character[i];
      pos++;
      if(pos==3){
      pos=0;
-     }
-     if(document.getElementById(array[0]).innerHTML.includes(x)=false){
-     document.getElementById(array[0]).innerHTML="";
-     }	     
-     if(document.getElementById(array[1]).innerHTML.includes(x)=false){
-     document.getElementById(array[1]).innerHTML="";
-     }	
-     if(document.getElementById(array[2]).innerHTML.includes(x)=false){
-     document.getElementById(array[2]).innerHTML="";
-     }	
-	     
+     }     
      } else {
      //Not in the array
      document.getElementById(array[pos]).setAttribute("href",x);
