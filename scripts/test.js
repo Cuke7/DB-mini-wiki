944c378b276d1f71aa7332e921826e024461d2bb
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2017-08-21 12:33:32 +0200

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,7 @@
 Table tab;
 
 tab = loadTable("data/char2.csv", "header");
-TableRow row = tab.getRow(0);
+TableRow row = tab.getRow(2);
 
 void display(){
   document.getElementById(array[0]).innerHTML=row.getString(0);  
