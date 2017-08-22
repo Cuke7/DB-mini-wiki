b7ee36ea3c4db7c1080f9afcf0a6a74715480505
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2017-08-22 16:14:59 +0200

    Update display.js

--- scripts/display.js
+++ scripts/display.js
@@ -2,13 +2,3 @@ function focus(){
  var element_to_scroll_to = document.getElementById('label1');
  element_to_scroll_to.scrollIntoView();
 }
-
-function focus2(){ 
- var element_to_scroll_to = document.getElementById('label2');
- element_to_scroll_to.scrollIntoView();
-}
-
-function focus3(){
- var element_to_scroll_to = document.getElementById('label3');
- element_to_scroll_to.scrollIntoView();
-}
