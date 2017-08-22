b7ab089df1b21c56590f86941a8a7a7106c13b75
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2017-08-22 09:21:15 +0200

    Create search2.js

---
+++ scripts/search2.js
@@ -0,0 +1,54 @@
+function search() {
+	document.getElementById(array2[0]).innerHTML="";
+	document.getElementById(array2[1]).innerHTML="";
+	document.getElementById(array2[2]).innerHTML="";
+	document.getElementById(array2[3]).innerHTML="";
+	document.getElementById(array2[4]).innerHTML="";	
+    var pos=0;
+    var x = document.getElementById("myText2").value.toLowerCase();
+    for(var i=0;i<Name.length;i++){
+     if (Name[i].toLowerCase().includes(x) && x!="") {
+     //In the array!
+     document.getElementById(array2[pos]).setAttribute("href","Basic_units.html#"+Name[i]);
+     document.getElementById(array2[pos]).innerHTML=Name[i];
+     pos++;
+     if(pos==5){
+     pos=0;
+     }     
+     } else {
+     //Not in the array
+     document.getElementById(array2[pos]).setAttribute("href",x);
+     document.getElementById(array2[pos]).innerHTML="";
+     }
+}
+	if(x==""){
+		document.getElementById(array2[0]).innerHTML="";
+		document.getElementById(array2[1]).innerHTML="";
+		document.getElementById(array2[2]).innerHTML="";
+		document.getElementById(array2[3]).innerHTML="";
+		document.getElementById(array2[4]).innerHTML="";
+	}
+	
+	if(document.getElementById(array2[0]).innerHTML==""){
+		document.getElementById(array2[0]).innerHTML=document.getElementById(array2[1]).innerHTML;
+		document.getElementById(array2[1]).innerHTML="";
+	}
+	if(document.getElementById(array2[1]).innerHTML==""){
+		document.getElementById(array2[1]).innerHTML=document.getElementById(array2[2]).innerHTML;
+		document.getElementById(array2[2]).innerHTML="";
+	}
+	if(document.getElementById(array2[2]).innerHTML==""){
+		document.getElementById(array2[2]).innerHTML=document.getElementById(array2[3]).innerHTML;
+		document.getElementById(array2[3]).innerHTML="";
+	}
+	if(document.getElementById(array2[3]).innerHTML==""){
+		document.getElementById(array2[3]).innerHTML=document.getElementById(array2[4]).innerHTML;
+		document.getElementById(array2[4]).innerHTML="";
+	}
+	
+	for( var j=0;j<array2.length;j++){
+	if(document.getElementById(array2[j]).innerHTML.toLowerCase().includes(x)==false){
+		document.getElementById(array2[j]).innerHTML="";
+	}
+	}
+}
