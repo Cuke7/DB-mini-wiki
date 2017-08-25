423333605a118ed98369972696fe7b0f447106ab
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2017-08-25 08:52:59 +0200

    Update top_button.js

--- scripts/top_button.js
+++ scripts/top_button.js
@@ -2,15 +2,22 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         document.getElementById("myBtn").style.display = "block";
+        document.getElementById("myBtn2").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
+        document.getElementById("myBtn2").style.display = "none";
     }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+} 
+
+function menuFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
 } 
